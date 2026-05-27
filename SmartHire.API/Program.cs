9797c083afa94eedbfdbb151b55a8db3c37faf4e
Thirumalaiah Siriveri <tirumalaiah.shiriveri@gmail.com>
2026-05-28 00:31:02 +0530

    AI Implemetations

--- SmartHire.API/Program.cs
+++ SmartHire.API/Program.cs
@@ -71,10 +71,13 @@ builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
-using(var scope = app.Services.CreateScope())
+if (!app.Environment.IsEnvironment("Testing"))
 {
-    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    await db.Database.MigrateAsync();
+    using (var scope = app.Services.CreateScope())
+    {
+        var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+        await db.Database.MigrateAsync();
+    }
 }
 
 // Configure the HTTP request pipeline.
--- SmartHire.Tests/Infrastructure/StartupTests.cs
+++ SmartHire.Tests/Infrastructure/StartupTests.cs
@@ -1,15 +1,40 @@
-﻿using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.VisualStudio.TestPlatform.TestHost;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SmartHire.Infrastructure.Data;
 
 namespace SmartHire.Tests.Infrastructure
 {
     public class StartupTests : IClassFixture<WebApplicationFactory<Program>>
     {
         private readonly WebApplicationFactory<Program> _factory;
+
         public StartupTests(WebApplicationFactory<Program> factory)
         {
-            _factory = factory;
+            _factory = factory.WithWebHostBuilder(builder =>
+            {
+                builder.UseEnvironment("Testing");
+                builder.ConfigureServices(services =>
+                {
+                    // Remove the real DbContext registration
+                    var descriptor = services.SingleOrDefault(
+                        d => d.ServiceType == typeof(DbContextOptions<AppDbContext>));
+
+                    if (descriptor != null)
+                    {
+                        services.Remove(descriptor);
+                    }
+
+                    // Add In-Memory database for testing
+                    services.AddDbContext<AppDbContext>(options =>
+                    {
+                        options.UseInMemoryDatabase("InMemoryAppDbForTesting");
+                    });
+                });
+            });
         }
+
         [Fact]
         public async Task Application_Should_Start_Successfully()
         {
